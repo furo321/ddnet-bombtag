f81aea9aa9d86788db9650f6e8b68b3337f244b5
furo321 <urof@disroot.org>
2023-11-07 01:04:43 +0100

    Add missing header guards

--- src/game/server/gamemodes/Bomb.h
+++ src/game/server/gamemodes/Bomb.h
@@ -1,6 +1,6 @@
 /* (c) Shereef Marzouk. See "licence DDRace.txt" and the readme.txt in the root of the distribution for more information. */
-#ifndef GAME_SERVER_GAMEMODES_DDRACE_H
-#define GAME_SERVER_GAMEMODES_DDRACE_H
+#ifndef GAME_SERVER_GAMEMODES_BOMB_H
+#define GAME_SERVER_GAMEMODES_BOMB_H
 
 #include <game/server/gamecontroller.h>
 #include <game/server/teams.h>
@@ -76,4 +76,4 @@ public:
 
 	void OnHammerHit(int ClientID, int TargetID) override;
 };
-#endif // GAME_SERVER_GAMEMODES_DDRACE_H
+#endif // GAME_SERVER_GAMEMODES_BOMB_H
--- src/game/server/gamemodes/bomb_config_variables.h
+++ src/game/server/gamemodes/bomb_config_variables.h
@@ -1,3 +1,6 @@
+#ifndef GAME_SERVER_GAMEMODES_BOMB_CONFIG_VARIABLES_H
+#define GAME_SERVER_GAMEMODES_BOMB_CONFIG_VARIABLES_H
+
 // def, min, max
 MACRO_CONFIG_INT(BombtagBombsPerPlayer, bombtag_bombs_per_player, 6, 1, 64, CFGFLAG_SERVER, "The amount of bombs that should spawn per X alive players.")
 MACRO_CONFIG_INT(BombtagScoreForSuriving, bombtag_score_for_surviving, 0, 0, 100, CFGFLAG_SERVER, "The amount of score given for players surviving a round.")
@@ -6,3 +9,5 @@ MACRO_CONFIG_INT(BombtagScoreForWinning, bombtag_score_for_winning, 1, 0, 100, C
 MACRO_CONFIG_INT(BombtagSecondsToExplosion, bombtag_seconds_to_explosion, 15, 0, 100, CFGFLAG_SERVER, "The amount of seconds till the bomb explodes.")
 MACRO_CONFIG_INT(BombtagBombDamage, bombtag_bomb_damage, 1, 0, 100, CFGFLAG_SERVER, "The amount of seconds removed from a bombs timer when hit by someone.")
 MACRO_CONFIG_INT(BombtagHammerFreeze, bombtag_hammer_freeze, 1, 0, 100, CFGFLAG_SERVER, "The amount of seconds of freeze inflicted when hitting an alive player. (0 to disable)")
+
+#endif // GAME_SERVER_GAMEMODES_BOMB_CONFIG_VARIABLES_H
